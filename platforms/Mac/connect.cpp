39ee057fe1022cb2b792ec21dc4909c17211ea06
Nick Frey <nickfrey123@gmail.com>
2012-06-02 18:20:26 -0500

    Mac update, added device scanner & socket connections

--- platforms/Mac/connect.cpp
+++ platforms/Mac/connect.cpp
@@ -1,3 +1,9 @@
+//
+//  connect.cpp
+//  Insidious
+//
+
+#import "connect.hpp"
 #include <iostream>
 #include <sys/types.h>   // Types used in sys/socket.h and netinet/in.h
 #include <netinet/in.h>  // Internet domain address structures and functions
@@ -6,50 +12,50 @@
 #include <unistd.h>
 #include <errno.h>
 
-using namespace std;
-
-main()
-{
-   int socketHandle;
-
-   // create socket
-
-   if((socketHandle = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) < 0)
-   {
-      close(socketHandle);
-      exit(EXIT_FAILURE);
-   }
-
+void SocketConnect::openSocket() {
+    int socketHandle;
+    
+    // create socket
+    if((socketHandle = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) < 0)
+    {
+        fprintf(stderr, "Could not create socket handle.\n");
+        close(socketHandle);
+        return;
+    }
+    
 	struct sockaddr_in remoteSocketInfo;
 	struct hostent *hPtr;
-	int socketHandle;
-	char *remoteHost="173.26.250.104";
+
+	char *remoteHost = "173.26.250.104";
 	int portNumber = 5555;
-
+    
 	bzero(&remoteSocketInfo, sizeof(sockaddr_in));  // Clear structure memory
-
+    
 	// Get system information
-
 	if((hPtr = gethostbyname(remoteHost)) == NULL)
 	{
-	   cerr << "System DNS name resolution not configured properly." << endl;
-	   cerr << "Error number: " << ECONNREFUSED << endl;
-	   exit(EXIT_FAILURE);
+        fprintf(stderr, "System DNS name resolution not configured properly.\n");
+        fprintf(stderr, "Error number: %d\n", ECONNREFUSED);
+        return;
 	}
-
+    
 	// Load system information for remote socket server into socket data structures
-
 	memcpy((char *)&remoteSocketInfo.sin_addr, hPtr->h_addr, hPtr->h_length);
 	remoteSocketInfo.sin_family = AF_INET;
 	remoteSocketInfo.sin_port = htons((u_short)portNumber);      // Set port number
-
-	if( (connect(socketHandle, (struct sockaddr *)&remoteSocketInfo, sizeof(sockaddr_in)) < 0)
-	{
-	   close(socketHandle);
-	   exit(EXIT_FAILURE);
-	}
-
-	close(socketHandle);
-       
+    
+    fprintf(stderr, "Attempting socket connection...\n");
+    if(connect(socketHandle, (struct sockaddr *)&remoteSocketInfo, sizeof(sockaddr_in)) < 0)
+    {
+        fprintf(stderr, "Could not open socket.\n");
+        close(socketHandle);
+        return;
+    }
+    
+    // Socket connected
+    fprintf(stderr, "Socket connected.\n");
+    // Do stuff...
+    
+    // Finished with socket
+    close(socketHandle);
 }
-          
