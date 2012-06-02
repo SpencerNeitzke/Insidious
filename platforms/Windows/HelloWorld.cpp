754002aa347c4937e1d54b9739ddb0db098e32d9
Nick Frey <nickfrey123@gmail.com>
2012-06-01 20:08:22 -0500

    Reorganize directory structure

---
+++ platforms/Windows/HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main ()	{
+	char input;
+	cout << "Spencer Neitzke" << endl;
+	cout << "Hello World!" << endl;
+	Sleep(2000);
+	cout << "A greeting was not what you expected." << endl;
+	Sleep(3000);
+	cout << "This is only the beginning" << endl;
+	Sleep(4000);
+	cout << endl << "Press enter to begin" << endl;
+	cin >> input;
+	return 0;
+}
